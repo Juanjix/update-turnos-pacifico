ba21692aac40a1e1402f024c5d55dbd0cb66f6a0
Juan José Peralta <juanjose.peralta92@gmail.com>
2026-05-03 00:15:42 -0300

    fix route name in turnos panel

--- .next/types/app/(dashboard)/mis-turnos/page.ts
+++ .next/types/app/(dashboard)/mis-turnos/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/juanso/Downloads/pacifico-tenis/app/page.tsx
-import * as entry from '../../../app/page.js'
+// File: /Users/juanso/Downloads/pacifico-tenis/app/(dashboard)/mis-turnos/page.tsx
+import * as entry from '../../../../../app/(dashboard)/mis-turnos/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/page.js')
+type TEntry = typeof import('../../../../../app/(dashboard)/mis-turnos/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
